17aa88331a599d21fa326c421a76a3b25d61d71a
WaraKaren <wkpacajesm@gmail.com>
2023-08-15 20:58:49 -0400

    cambio de menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+document.addEventListener('DOMContentLoaded', function () {
+  function printdata(array) {
+    const container = document.querySelector('.movie-grid');
+    let html = ''; // Variable para almacenar el HTML
+  
+    for (let i = 0; i < array.length; i++) {
+      html += ` <figure>
+          <img
+            src="${array[i].poster}"
+            alt="${array[i].title}"
+          />
+          <figcaption>${array[i].title}</figcaption>
+        </figure>`;
+    }
+    container.innerHTML = html; // Agregar el HTML al contenedor
+  }
+  
+  // Llamar a la función printdata() después de que el DOM esté cargado
+  printdata(data.films);
+});
+  
