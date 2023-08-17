f9cc4ee7612d7d72b772e3ab4b506b92b9a15968
mariajose <mariajosellanos-14@hotmail.com>
2023-08-17 12:17:41 -0500

    avance de mi codigo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
+//import { data} from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+/* const searchTitle = 'rick and morty';
+const foundFilm = data.films.find((film) => film.title === searchTitle);
+const container = document.querySelector('todomenu')
+for (let i = 0; i < data.films.length; i++) {
+    console.log(data.films[i]);
+    container.innerHTML += `<figure>
+    <img
+    src=" ${data.films[i].poster}"
+    alt= "${data.films[i].title}"
+    <figcaption>${data.films[i].title} <figcaption>
+    </figure>`
+} */
+
+
